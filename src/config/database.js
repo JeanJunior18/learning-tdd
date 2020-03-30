149bc64fd686a15731f2b0b34991ed1e21058318
Jean Junior <jclgjunior@gmail.com>
2020-03-30 12:17:23 -0300

    Getting Start

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  host: 'localhost',
+  username: 'root',
+  password: 'root',
+  dialect: 'mysql',
+  database: 'tdd-learning',
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
